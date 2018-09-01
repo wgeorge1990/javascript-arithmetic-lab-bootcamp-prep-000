9a62117a235c8178a87ee2f40e096a2b47f5902f
williameverettgeorge@icloud.com <williameverettgeorge@icloud.com>
2018-09-01 19:13:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = Math.floor(Math.random() * 1000)
+let b = Math.floor(Math.random() * 1000)
+function add(a, b){
+return a+b;
+}
+add(a, b);
+
+function subtract(a, b){
+  return a-b;
+}
+subtract(a, b);
