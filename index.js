bdb6d9fa69b6472ea23e7e68068f21a2d6ed79c3
williameverettgeorge@icloud.com <williameverettgeorge@icloud.com>
2018-09-01 19:38:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let a = Math.floor(Math.random() * 1000)
-let b = Math.floor(Math.random() * 1000)
+let a = Math.floor(Math.random() * 1000);
+let b = Math.floor(Math.random() * 1000);
 function add(a, b){
 return a+b;
 }
